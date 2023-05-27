5a57a2bbdf4a3724b8baf54d087c0d6de33dadf3
Daffa Riza Muliya <daffamuliya15@gmail.com>
2023-05-28 06:11:22 +0700

    nodemon

--- app.js
+++ app.js
@@ -11,7 +11,21 @@ const tipsRoutes = require('./api/routes/tips');
 // });
 
 app.use('/artikel', artikelRoutes);
-app.use('/tips', tipsRoutes)
+app.use('/tips', tipsRoutes); 
 
+app.use((req, res, next) => {
+    const error = new Error('Halaman tidak ada');
+    error.status(404);
+    next(error);
+})
+
+app.use((error, req, res, next) => {
+    res.status(error.status || 500);
+    res.json({
+        error: {
+            message: error.message
+        }
+    });
+});
 
 module.exports = app;
